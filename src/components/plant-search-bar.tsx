fe6b2362bfb8de232c1f05dc0c6f47a78e644d59
Krigle <130289154+Krigle@users.noreply.github.com>
2024-07-31 18:13:35 +0100

    Better search bar functionality - able to click outside search bar without clearing the search field whilst the dropdown disappears

--- src/components/plant-search-bar.tsx
+++ src/components/plant-search-bar.tsx
@@ -1,35 +1,38 @@
 "use client";
-import { useSearchContext, usePlantContext } from "@/lib/hooks";
+import { useRef, useEffect } from "react";
+import { usePlantContext, useSearchContext } from "@/lib/hooks";
 import { Search } from "lucide-react";
 import SearchResultsDropdown from "./search-results-dropdown";
-import { useEffect, useRef } from "react";
 
 export default function PlantSearchBar() {
-  const { results, searchQuery, handleChangeSearchQuery } = useSearchContext();
+  const {
+    searchQuery,
+    handleChangeSearchQuery,
+    results,
+    isDropdownVisible,
+    setIsDropdownVisible,
+  } = useSearchContext();
   const { handleAddPlant } = usePlantContext();
-  const dropdownRef = useRef(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
-  // Handle click outside the dropdown to close it
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
-        dropdownRef.current &&
-        (dropdownRef.current as HTMLElement).contains(event.target as Node)
+        wrapperRef.current &&
+        !wrapperRef.current.contains(event.target as Node)
       ) {
-        // If clicked outside the dropdown, clear the search query
-        handleChangeSearchQuery("");
+        setIsDropdownVisible(false);
       }
     };
 
-    // Add event listener to detect clicks outside
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      // Clean up the event listener on component unmount
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [handleChangeSearchQuery]);
+  }, [wrapperRef, setIsDropdownVisible]);
+
   return (
-    <div className="relative w-[500px] h-12">
+    <div ref={wrapperRef} className="relative w-[500px] h-12">
       <form
         className="w-full h-full flex items-center"
         onSubmit={(e) => e.preventDefault()}
@@ -50,14 +53,9 @@ export default function PlantSearchBar() {
           <Search size={24} />
         </button>
       </form>
-      <div ref={dropdownRef}>
-        {searchQuery.length > 0 && (
-          <SearchResultsDropdown
-            results={results}
-            onAddPlant={handleAddPlant}
-          />
-        )}
-      </div>
+      {isDropdownVisible && searchQuery && (
+        <SearchResultsDropdown results={results} onAddPlant={handleAddPlant} />
+      )}
     </div>
   );
 }
