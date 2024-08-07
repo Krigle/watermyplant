b94a074c5d3dd6bfae220a891883532286fb424d
Krigle <130289154+Krigle@users.noreply.github.com>
2024-08-07 20:18:52 +0100

    Added delete function to the add note feature

--- src/components/note-details.tsx
+++ src/components/note-details.tsx
@@ -1,10 +1,20 @@
+import { usePlantContext } from "@/lib/hooks";
 import { PlantNote } from "@/lib/types";
+import { Trash2 } from "lucide-react";
 
 export default function NoteDetails({ note }: { note: PlantNote }) {
+  const { handleDeleteNote } = usePlantContext()!;
   return (
-    <div>
-      <p>{note.date}</p>
-      <p>{note.note}</p>
+    <div className="p-4 border rounded-md shadow-md bg-white">
+      <p className="font-semibold text-lg">{note.date}</p>
+      <p className="mt-2 text-gray-600">{note.note}</p>
+      {note.note && (
+        <div className="mt-4 flex justify-end">
+          <button onClick={() => handleDeleteNote(note.id)}>
+            <Trash2 className="text-red-500 cursor-pointer" />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
